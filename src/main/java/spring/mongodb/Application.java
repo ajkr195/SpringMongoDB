6a7a7b351c95b3ac64b4de053c26fe8f05c67c8c
ajay <ajkr195@gmail.com>
2020-04-04 10:45:00 -0500

    update

--- src/main/java/spring/mongodb/Application.java
+++ src/main/java/spring/mongodb/Application.java
@@ -1,5 +1,8 @@
 package spring.mongodb;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +10,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		disableAccessWarnings();
 		SpringApplication.run(Application.class, args);
+	}
+	
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void disableAccessWarnings() {
+		try {
+			Class unsafeClass = Class.forName("sun.misc.Unsafe");
+			Field field = unsafeClass.getDeclaredField("theUnsafe");
+			field.setAccessible(true);
+			Object unsafe = field.get(null);
+
+			Method putObjectVolatile = unsafeClass.getDeclaredMethod("putObjectVolatile", Object.class, long.class,
+					Object.class);
+			Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
+
+			Class loggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
+			Field loggerField = loggerClass.getDeclaredField("logger");
+			Long offset = (Long) staticFieldOffset.invoke(unsafe, loggerField);
+			putObjectVolatile.invoke(unsafe, loggerClass, offset, null);
+		} catch (Exception ignored) {
+		}
 	}
 }
